9a7b68df86decd1c39329110cb33ab47925bdada
Santiago Carot-Nemesio <sancane@gmail.com>
2009-12-29 18:18:57 +0100

    Working in l2cap input stream buffered packet for hdp connections

--- src/es/libresoft/hdp/HDPInputStream.java
+++ src/es/libresoft/hdp/HDPInputStream.java
@@ -11,22 +11,45 @@ public class HDPInputStream extends InputStream{
 		
 		protected byte buff[];
 		protected int coun;
-		protected int mark;
 		protected int pos;
 		
-		private static final int L2CAP_DEFAULT_MTU = 672;
+		private static final int L2CAP_DEFAULT_MTU = 6; /* TODO: Value used to test change for 672 */ //672;
 		
-		HDPInputStream(HDPSession session, long dc){
+		private InputStream in; /*Used only for testing without connect the intput stream to a L2CAP socket*/
+		
+		public HDPInputStream(HDPSession session, long dc){
 			this.session = session;
 			this.dcd = dc;
 			buff = new byte[L2CAP_DEFAULT_MTU];
 			coun = 0;
-			mark = 0;
 			pos = 0;
 		}
 
+		public HDPInputStream(InputStream input){
+			in = input;
+			buff = new byte[L2CAP_DEFAULT_MTU];
+			coun = 0;
+			pos = 0;
+		}
+		
 		@Override
 		public int read() throws IOException {
+			if (pos >= coun) {
+				int ret;
+				/* get next packet */
+				//ret = session.read(dcd, buff, 0, L2CAP_DEFAULT_MTU);
+				ret = in.read(buff, 0, L2CAP_DEFAULT_MTU);
+				if (ret <= 0)
+					return -1;
+				coun = ret;
+				pos = 0;
+				//System.out.println("leidos: " + coun + ", pos: " + pos);
+			}
+			
+			//System.out.println("coun: " + coun + ", pos: " + pos);
+			
+			return (int)buff[pos++] & 0xff;
+			/*
 			byte b[] = new byte[1];
 	        int ret = session.read(dcd, b, 0, 1);
 	        if (ret == 1) {
@@ -34,12 +57,41 @@ public class HDPInputStream extends InputStream{
 	        } else {
 	            return -1;
 	        }
+	        */
 		}
 
 		@Override
 		public int read(byte[] b, int offset, int length) throws IOException {
-			System.out.println("READ with array");
-			return session.read(dcd, b, offset, length);
-		}		
+			int r;
+			if (pos >= coun) {
+				/* get next packet */
+				//r = session.read(dcd, buff, 0, L2CAP_DEFAULT_MTU);
+				r = in.read(buff, 0, L2CAP_DEFAULT_MTU);
+				if (r <= 0)
+					return r;
+				coun = r;
+				pos = 0;
+			}
+			
+			r = coun - pos;
+			if (r >= length) {
+				System.arraycopy(buff, pos, b, offset, length);
+				pos = pos + length;
+				return length;
+			}			
+			
+			System.arraycopy(buff, pos, b, offset, r);
+			pos = coun;
+			return r;
+		}
 		
+		@Override
+		public int available() throws IOException {
+			return coun - pos; 
+		}
+
+		@Override
+		public boolean markSupported() {
+			return false;
+		}		
 }
