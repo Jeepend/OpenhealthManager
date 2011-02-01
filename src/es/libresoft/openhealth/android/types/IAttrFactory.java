da855d7dcbd5e7f8a0c57f73acd0f1e93912cf28
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2011-02-01 09:34:16 +0100

    Fix some bugs sending an IAttribute

--- src/es/libresoft/openhealth/android/types/IAttrFactory.java
+++ src/es/libresoft/openhealth/android/types/IAttrFactory.java
@@ -5,21 +5,19 @@ import ieee_11073.part_20601.asn1.HANDLE;
 
 public class IAttrFactory {
 
-	public static final IAttribute parcelHANDLE (HANDLE handle) {
-		IAttribute iattr = new IAttribute();
+	public static final void parcelHANDLE (HANDLE handle, IAttribute attr) {
 		IHANDLE ihandle = new IHANDLE();
 		ihandle.setHandle(handle.getValue().getValue());
-		iattr.setAttr(ihandle);
-		return iattr;
+		attr.setAttr(ihandle);
 	}
 
 	public static final void getParcelableAttribute (Object asnAttr, IAttribute attr) {
-		attr = null;
 
-		if (asnAttr instanceof HANDLE)
-			attr = parcelHANDLE((HANDLE) asnAttr);
+		if (asnAttr instanceof HANDLE) {
+			parcelHANDLE((HANDLE) asnAttr, attr);
+			return;
+		}
 
-		if (attr == null)
-			System.err.println("Unknown method provided. Can't create parcelable attribute.");
+		System.err.println("Unknown method provided. Can't create parcelable attribute.");
 	}
 }
