02689752b81a5eef57ebcdff69d097894091cf72
Santiago Carot-Nemesio <sancane@gmail.com>
2011-02-01 10:59:51 +0100

    Change license in Timeout

--- src/ieee_11073/part_20601/phd/dim/TimeOut.java
+++ src/ieee_11073/part_20601/phd/dim/TimeOut.java
@@ -1,8 +1,7 @@
 /*
-Copyright (C) 2008-2009  Santiago Carot Nemesio
-email: scarot@libresoft.es
+Copyright (C) 2010 GSyC/LibreSoft, Universidad Rey Juan Carlos.
 
-Author: José Antonio Santos Cadenas <jcaden@libresoft.es>
+Author: Jose Antonio Santos Cadenas <jcaden@libresoft.es>
 
 This program is a (FLOS) free libre and open source implementation
 of a multiplatform manager device written in java according to the
