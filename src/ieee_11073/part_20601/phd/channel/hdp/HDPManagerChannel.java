c42cc2c9f95d9affec3d0ef29b47e79109f2d8d0
Jorge Fernandez Gonzalez <jorge.fernandez.gonzalez@gmail.com>
2012-02-08 18:17:19 +0100

    Check for HDP Source Profile among the bonded devices.

--- src/ieee_11073/part_20601/phd/channel/hdp/HDPManagerChannel.java
+++ src/ieee_11073/part_20601/phd/channel/hdp/HDPManagerChannel.java
@@ -26,15 +26,25 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package ieee_11073.part_20601.phd.channel.hdp;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Set;
+import java.util.UUID;
+
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothHealth;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
+import android.os.ParcelUuid;
 import android.widget.Toast;
+import es.libresoft.openhealth.Agent;
 import es.libresoft.openhealth.logging.Logging;
 
 public class HDPManagerChannel {
 	private static String TAG = "HDPManagerChannel";
+
+	private static UUID HDP_SOURCE = UUID.fromString("00001401-0000-1000-8000-00805f9b34fb");
 
 	private Context context;
 	private BluetoothAdapter mBluetoothAdapter;
@@ -63,7 +73,12 @@ public class HDPManagerChannel {
 			return;
 		}
 
-		Logging.info("TODO: ieee_11073.part_20601.phd.channel.hdp.HDPManagerChannel check HDP profiles and register apps");
+		Set<BluetoothDevice> btDevices = mBluetoothAdapter.getBondedDevices();
+		for (BluetoothDevice device: btDevices){
+			checkHDPProfile(device);
+		}
+
+		Logging.info("TODO: ieee_11073.part_20601.phd.channel.hdp.HDPManagerChannel register applications");
 	}
 
 	public void finish() {
@@ -86,5 +101,42 @@ public class HDPManagerChannel {
 			}
 		}
 	};
+
+	private void checkHDPProfile(BluetoothDevice device){
+		try {
+			/*
+			 * Check for HDP Source profile (0x1401)
+			 */
+			Method m = device.getClass().getMethod("getUuids");
+			ParcelUuid[] uuids = (ParcelUuid[])m.invoke(device);
+
+			for (int i = 0; i < uuids.length; i++){
+				if (uuids[i].getUuid().equals(HDP_SOURCE)){
+					Logging.debug(TAG + " - The device " + device.getAddress() + " contains HDP Profile");
+
+					/*
+					 * The description of the agent will be on the format MAC/feature
+					 * being feature the number of the corresponding profile (IEEE 11073)
+					 * e.g. 4100 for the pulsioximeter
+					 * */
+					String description = device.getAddress() + "/ " + device.getBluetoothClass().getDeviceClass();
+					Logging.debug(TAG + " - Creating agent with description: " + description);
+
+					Agent a = new Agent(description);
+					HDPManagedAgents.getInstance().addAgent(a);
+				}
+			}
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
